c07318a0d1de1359bed16afc95bc14d92da6afb8
Ilovepunchnakub <somazuei1@gmail.com>
2026-04-23 15:40:21 +0700

    Delete scripts directory

--- scripts/loveLetter/content.js
+++
@@ -1,4 +0,0 @@
-// ===== คำอธิบายไฟล์ (ภาษาไทย) : scripts/loveLetter/content.js =====
-import { TEXT_CONTENT } from '../siteTextContent.js';
-
-export const LOVE_LETTER_CONTENT = TEXT_CONTENT.data.loveLetter;
